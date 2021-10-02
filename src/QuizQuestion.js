e7090399fbbd72cb9d5e6670509a3ce05d1c5323
Andrew Brenner <abrenn977@cable.comcast.com>
2021-10-02 19:00:14 -0400

    Displays error message when incorrect

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,12 +4,19 @@ import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          incorrectAnswer: false,
+        };
+
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(buttonText) {
       if(buttonText === this.props.quiz_question.answer) {
+        this.setState({incorrectAnswer: false});
         this.props.showNextQuestionHandler();
+      } else {
+        this.setState({incorrectAnswer: true});
       }
     }
 
@@ -17,6 +24,7 @@ class QuizQuestion extends Component {
 
         return (
             <main>
+            {this.state.incorrectAnswer && <p className='error'>Sorry, that's not right</p>}
             <section>
               <p>{this.props.quiz_question.instruction_text}</p>
             </section>
@@ -26,7 +34,7 @@ class QuizQuestion extends Component {
                   <QuizQuestionButton 
                     key={index} 
                     button_text={answer}
-                    clickHandler={this.handleClick}
+                    clickHandler={this.handleClick.bind(this)}
                   />
                 ))}
               </ul>
